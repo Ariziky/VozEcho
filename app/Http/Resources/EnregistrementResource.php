84dcf0726d524c17a21855d381fd96d0e7f7fd93
Khaled <khaledouroakpo@gmail.com>
2023-06-28 17:28:10 +0100

    Get basename of the file for path

--- app/Http/Resources/EnregistrementResource.php
+++ app/Http/Resources/EnregistrementResource.php
@@ -5,6 +5,7 @@ namespace App\Http\Resources;
 use App\Models\Enregistrement;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Str;
 
 /** @mixin Enregistrement */
 class EnregistrementResource extends JsonResource
@@ -12,7 +13,7 @@ class EnregistrementResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'uuid' => $this->uuid,
+            'path' => Str::afterLast(basename($this->path), '/'),
         ];
     }
 }
